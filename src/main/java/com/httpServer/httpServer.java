11ec0f56fc1b13c201a931425a0a1ef8fce60ecd
FruitPunchMisbah <misbahulhasan365@gmail.com>
2026-03-01 17:41:11 +0530

    first commit

---
+++ src/main/java/com/httpServer/httpServer.java
@@ -0,0 +1,9 @@
+package com.httpServer;
+
+/*Driver class for HTTP Server*/
+
+public class httpServer {
+    public static void main(String[] args) {
+        System.out.println("Server starting...");
+    }
+}
