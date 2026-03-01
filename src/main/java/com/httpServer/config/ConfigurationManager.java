98128272315a17974a010a5897cf4f71ac7b3e80
FruitPunchMisbah <misbahulhasan365@gmail.com>
2026-03-01 19:43:16 +0530

    finished setting up configuration manager

--- src/main/java/com/httpServer/config/ConfigurationManager.java
+++ src/main/java/com/httpServer/config/ConfigurationManager.java
@@ -1,6 +1,14 @@
 package com.httpServer.config;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.httpServer.util.Json;
+
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.net.http.HttpConnectTimeoutException;
 
 public class ConfigurationManager {
 
@@ -22,11 +30,43 @@ public class ConfigurationManager {
 
     /* Loads a configuration file by the path provided */
     public void loadConfigurationFile(String filePath) {
-        FileReader fileReader = new FileReader(filePath);
+        FileReader fileReader = null;
+        try{
+            fileReader = new FileReader(filePath);
+        }
+        catch(FileNotFoundException e){
+            throw new HttpConfigurationException(e);
+        }
+
         StringBuilder stringBuilder = new StringBuilder();
-        while((int i = FileReader.read()) != -1)
+
+        int i;
+        try {
+            while((i = fileReader.read()) != -1){
+                stringBuilder.append((char)i);
+            }
+        } catch (IOException e) {
+            throw new HttpConfigurationException(e);
+        }
+
+        JsonNode conf = null;
+        try {
+            conf = Json.parse(stringBuilder.toString());
+        } catch (IOException e) {
+            throw new HttpConfigurationException("Error parsing JSON configuration file", e);
+        }
+        try {
+            myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
+        } catch (JsonProcessingException e) {
+            throw new HttpConfigurationException("Error parsing the Configuration file internal",e);
+        }
     }
 
     /* Return the current loaded configuration */
-    public void getCurrentConfiguration(){}
+    public Configuration getCurrentConfiguration(){
+        if(myCurrentConfiguration == null){
+            throw new HttpConfigurationException("No Current Configuration Set.");
+        }
+        return myCurrentConfiguration;
+    }
 }
--- src/main/java/com/httpServer/httpServer.java
+++ src/main/java/com/httpServer/httpServer.java
@@ -2,8 +2,17 @@ package com.httpServer;
 
 /*Driver class for HTTP Server*/
 
+import com.httpServer.config.Configuration;
+import com.httpServer.config.ConfigurationManager;
+
 public class httpServer {
     public static void main(String[] args) {
         System.out.println("Server starting...");
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        System.out.println("Current Port: " + conf.getPort());
+        System.out.println("Current Webroot: " + conf.getWebroot());
     }
 }
