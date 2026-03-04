a3908920d2c22c0fc185d7bd2152316773322ed9
FruitPunchMisbah <misbahulhasan365@gmail.com>
2026-03-04 16:32:28 +0530

    started working on HTTP parsing protocols

---
+++ src/main/java/http/HttpParser.java
@@ -0,0 +1,20 @@
+package http;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class HttpParser {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpParser.class);
+
+    public void parseHttpRequest(InputStream inputStream){
+        InputStreamReader reader = new InputStreamReader(inputStream, StandardCharsets.US_ASCII);
+        parseRequestLine(inputStream);
+        parseReaders(inputStream);
+        parseBody(inputStream);
+    }
+}
